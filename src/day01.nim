b69ed70b34dd9e4dd69d4e22794027095f33703f
Ari Koivula <ari.koivula@solita.fi>
2021-12-01 15:54:53 +0200

    Add day01 solution 2

--- src/day01.nim
+++ src/day01.nim
@@ -1,4 +1,6 @@
 import std/strutils
+import std/sequtils
+
 
 proc countIncreasing() : int =
     let f = open("input/1")
@@ -15,4 +17,42 @@ proc countIncreasing() : int =
     
     return increasingCnt
 
+
+proc readToSeq(path: string): seq[int] =
+    let f = open(path)
+    defer: f.close()
+
+    var
+        numbers: seq[int]
+        line: string
+    while f.readLine(line):
+        numbers.add(parseInt(line))
+
+    return numbers
+
+
+proc sum(values: seq[int]): int =
+    return foldl(values, a + b, 0)
+
+
+proc countIncreasingWindow(windowSize: int): int = 
+    let numbers: seq[int] = readToSeq("input/1")
+    let size = numbers.len
+
+    assert windowSize <= size
+
+    var increasingCnt = 0
+
+    for i in 0 .. (size - windowSize - 1):
+        let a = numbers[i ..< i+windowSize]
+        let b = numbers[i+1 ..< i+1+windowSize]
+        if sum(a) < sum(b):
+            increasingCnt += 1
+
+    return increasingCnt
+
+
+assert countIncreasing() == countIncreasingWindow(1)
+
 echo(countIncreasing())
+echo(countIncreasingWindow(3))
