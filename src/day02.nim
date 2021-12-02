e9b260a13f8c7af20155200ddb1f2681e9b706df
Ari Koivula <ari.koivula@solita.fi>
2021-12-02 17:25:12 +0200

    Add day02 solution 2

--- src/day02.nim
+++ src/day02.nim
@@ -1,5 +1,6 @@
 import std/parseutils
 import std/strutils
+import sugar
 
 
 type
@@ -10,12 +11,13 @@ type
     Command = tuple
         name: CommandType
         value: int
-    Position = tuple
+    State = tuple
         up: int
         forward: int
+        aim: int
 
 
-proc parseCommand(line: string): Command =
+func parseCommand(line: string): Command =
     let parts = split(line, " ")
     let name = parseEnum[CommandType](parts[0])
     let value = parseInt(parts[1])
@@ -32,21 +34,38 @@ proc readToSec(path: string): seq[Command] =
         result.add(parseCommand(line))
 
 
-proc calcPosition(input: seq[Command], start: Position): Position =
+func moveAbsolute(cmd: Command, state: State): State =
+    result = state
+    case cmd.name:
+        of forward: result.forward += cmd.value
+        of up: result.up += cmd.value
+        of down: result.up -= cmd.value
+
+
+func moveRelative(cmd: Command, state: State): State =
+    result = state
+    case cmd.name:
+        of forward:
+            result.forward += cmd.value
+            result.up -= result.aim * cmd.value
+        of up: result.aim -= cmd.value
+        of down: result.aim += cmd.value
+
+
+func calcState(input: seq[Command], start: State, fun: (Command, State) -> State): State =
     result = start
     for cmd in input:
-        case cmd.name:
-            of forward: result.forward += cmd.value
-            of up: result.up += cmd.value
-            of down: result.up -= cmd.value
-    
-    echo(result)
+        result = fun(cmd, result)
 
 
 # Tests
-assert calcPosition(@[(up, 5), (down, 3), (forward, 11)], (0, 0)) == (2, 11)
-
+assert calcState(@[(up, 5), (down, 3), (forward, 11)], (0, 0, 0), moveAbsolute) == (2, 11, 0)
+assert calcState(@[(up, 2), (down, 4), (forward, 3)], (0, 0, 0), moveRelative) == (-6, 3, 2)
 
 let input = readToSec("input/2")
-let position = calcPosition(input, (up: 0, forward: 0))
-echo($position & " => " & $(position.up * position.forward))
+
+let state1 = calcState(input, (up: 0, forward: 0, aim: 0), moveAbsolute)
+echo("Problem 1: " & $state1 & " => " & $(-state1.up * state1.forward))
+
+let state2 = calcState(input, (up: 0, forward: 0, aim: 0), moveRelative)
+echo("Problem 2: " & $state2 & " => " & $(-state2.up * state2.forward))
