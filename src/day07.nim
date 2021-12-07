772778074f4d213ef35dc348d4e3a3a08880aeef
Ari Koivula <ari@koivu.la>
2021-12-07 09:27:23 +0200

    Add day07 solution 2

--- src/day07.nim
+++ src/day07.nim
@@ -26,6 +26,12 @@ func calcFuel(ages: Crabs, level: int): int =
         result += abs(pos - level) * cnt
 
 
+func calcExpFuel(ages: Crabs, level: int): int =
+    for pos, cnt in ages:
+        let n = abs(pos - level)
+        result += ((n * (n + 1)) div 2) * cnt
+
+
 proc parseInput(s: string): Crabs =
     result = newAges()
     for pos in split(s, {',', '\n', '\r'}):
@@ -42,12 +48,12 @@ func calcAvg(crabs: Crabs): int =
     return sumAll div cntAll
 
 
-func findLowestFuel(crabs: Crabs, start: int): int =
-    var bestLevel: int = start
-    var bestFuel: int = calcFuel(crabs, bestLevel)
+func findLowestFuel(crabs: Crabs, fun: (Crabs, int) -> int): int =
+    var bestLevel: int = 0
+    var bestFuel: int = fun(crabs, bestLevel)
     while true:
-        debugEcho("level ", bestLevel, " fuel ", bestFuel)
-        let nextFuel = calcFuel(crabs, bestLevel + 1)
+        #debugEcho("level ", bestLevel, " fuel ", bestFuel)
+        let nextFuel = fun(crabs, bestLevel + 1)
         if nextFuel >= bestFuel:
             break
         bestFuel = nextFuel
@@ -70,12 +76,27 @@ when isMainModule:
                 calcFuel(day0, 2) == 37
                 calcFuel(day0, 3) == 39
                 calcFuel(day0, 10) == 71
-                findLowestFuel(day0, 0) == 2
+                findLowestFuel(day0, calcFuel) == 2
+        
+        test "calcExpFuel":
+            let day0 = parseInput(readFile("input/7example"))
+            check:
+                calcExpFuel(day0, 0) == 290
+                calcExpFuel(day0, 2) == 206
+                calcExpFuel(day0, 5) == 168
+                findLowestFuel(day0, calcExpFuel) == 5
 
 when isMainModule:
     block:
         let cpu = cpuTime()
         let crabs = parseInput(readFile("input/7"))
-        let level = findLowestFuel(crabs, 0)
+        let level = findLowestFuel(crabs, calcFuel)
         echo("Lowest fuel level: ", level, ", fuel: ", calcFuel(crabs, level))
+        echo("time: ", cpuTime() - cpu)
+
+    block:
+        let cpu = cpuTime()
+        let crabs = parseInput(readFile("input/7"))
+        let level = findLowestFuel(crabs, calcExpFuel)
+        echo("Lowest expFuel level: ", level, ", fuel: ", calcExpFuel(crabs, level))
         echo("time: ", cpuTime() - cpu)
