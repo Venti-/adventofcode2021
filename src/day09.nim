9ece8b586d24ef77c29005e2f9cec13ac2cf466a
Ari Koivula <ari.koivula@solita.fi>
2021-12-09 22:14:46 +0200

    Add day09 solution 2

--- src/day09.nim
+++ src/day09.nim
@@ -1,16 +1,13 @@
 import std/strutils
-import std/parseutils
 import std/sequtils
 import std/streams
-import std/tables
 import std/times
-import std/options
-import sugar
+import std/sets
+import std/algorithm
 
 
 type
-    CaveRow = seq[int]
-    Cave = seq[CaveRow]
+    Cave = seq[seq[int]]
     Point = tuple[y: int, x: int]
 
 
@@ -26,34 +23,57 @@ proc readInput(input: string): seq[seq[int]] =
             .map(readLine)
 
 
-func getHeight(cave: Cave, point: Point): Option[int] =
+func getHeight(cave: Cave, point: Point): int =
+    return cave[point.y][point.x]
+
+
+iterator neighbors(cave: Cave, point: Point): Point =
     let (y, x) = point
-    if y < 0 or y > cave.high:
-        return none(int)
-    let row = cave[y]
-    if x < 0 or x > row.high:
-        return none(int)
-    return some(row[x])
+    let (minY, minX) = (cave.low, cave[0].low)
+    let (maxY, maxX) = (cave.high, cave[0].high)
+    if y > minY:
+        yield (y - 1, x)
+    if y < maxY:
+        yield (y + 1, x)
+    if x > minX:
+        yield (y, x - 1)
+    if x < maxX:
+        yield (y, x + 1)
 
 
 func isLowPoint(cave: Cave, point: Point): bool =
-    let (y, x) = point
-    let neighbors = [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)]
-    let height = cave.getHeight((y, x)).get()
-    for p in neighbors:
+    let height = cave.getHeight(point)
+    for p in cave.neighbors(point):
         let otherHeight = cave.getHeight(p)
-        if otherHeight.isSome() and otherHeight.get() <= height:
+        if otherHeight <= height:
             return false
     return true
 
 
-func filter(cave: Cave, fun: (Cave, Point) -> bool): iterator(): int =
-    return iterator(): int =
-        for y in cave.low .. cave.high:
-            let row = cave[y]
-            for x in row.low .. row.high:
-                if fun(cave, (y, x)):
-                    yield cave[y][x]
+iterator points(cave: Cave): Point =
+    for y in cave.low .. cave.high:
+        let row = cave[y]
+        for x in row.low .. row.high:
+            yield (y, x)
+
+
+func floodFill(cave: Cave, start: Point, height: int): seq[Point] =
+    var toVisit: HashSet[Point]
+    var visited: HashSet[Point]
+    toVisit.incl(start)
+    while toVisit.len > 0:
+        let curPoint = toVisit.pop()
+        visited.incl(curPoint)
+        for adjacentPoint in cave.neighbors(curPoint):
+            let adjacentHeight = cave.getHeight(adjacentPoint)
+            if adjacentHeight < height and not visited.contains(adjacentPoint):
+                toVisit.incl(adjacentPoint)
+    
+    return visited.toSeq()
+
+
+func basinSize(cave: Cave, point: Point): int =
+    return floodFill(cave, point, 9).len
 
 
 when isMainModule:
@@ -64,7 +84,7 @@ when isMainModule:
             check:
                 cave == @[@[1, 2, 3], @[4, 5, 6], @[7, 8, 9]]
 
-        test "read example":
+        test "Example 1":
             let cave = readInput(readFile("input/9example"))
             check:
                 not cave.isLowPoint((0, 0))
@@ -73,10 +93,36 @@ when isMainModule:
                 cave.isLowPoint((2, 2))
                 cave.isLowPoint((4, 6))
                 not cave.isLowPoint((4, 9))
-                cave.filter(isLowPoint).mapIt(it + 1).foldl(a + b) == 15
+                cave.points().toSeq().filterIt(cave.isLowPoint(it)).mapIt(cave.getHeight(it) + 1).foldl(a + b) == 15
+
+        test "Example 2":
+            let cave = readInput(readFile("input/9example"))
+            check:
+                cave.basinSize((0, 0)) == 3
+                cave.basinSize((0, 9)) == 9
+                cave.basinSize((2, 2)) == 14
+                cave.basinSize((4, 6)) == 9
 
 
 when isMainModule:
-    let cave = readInput(readFile("input/9"))
-    let riskLevelSum = cave.filter(isLowPoint).mapIt(it + 1).foldl(a + b)
-    echo(riskLevelSum)
+    template benchmark(code: untyped) =
+        block:
+            let cpu = cpuTime()
+            code
+            echo("cpuTime: ", cpuTime() - cpu)
+
+    benchmark:
+        let cave = readInput(readFile("input/9"))
+        let riskLevelSum = cave.points
+                .toSeq()
+                .filterIt(cave.isLowPoint(it))
+                .mapIt(cave.getHeight(it) + 1)
+                .foldl(a + b)
+        echo("Solution 1: ", riskLevelSum)
+
+    benchmark:
+        let cave = readInput(readFile("input/9"))
+        let lowPoints = cave.points().toSeq().filterIt(cave.isLowPoint(it))
+        let orderedSizes = lowPoints.mapIt(cave.basinSize(it)).sorted(system.cmp[int], SortOrder.Descending)
+        let solution = orderedSizes[0 .. 2].toSeq().foldl(a * b, 1)
+        echo("Solution 2: ", solution)
