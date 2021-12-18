78e4196f59346d016b2217b2d58cc42f926c075d
Ari Koivula <ari@koivu.la>
2021-12-18 04:05:02 +0200

    Add day17 solution 2

--- src/day17.nim
+++ src/day17.nim
@@ -30,7 +30,7 @@ func step(probe: Probe): Probe =
     result.position.x += probe.velocity.x
     if probe.velocity.x > 0:
         result.velocity.x -= 1
-    else:
+    elif probe.velocity.x < 0:
         result.velocity.x += 1
     result.velocity.y -= 1
 
@@ -58,6 +58,27 @@ func findHighestYVelocity(area: Area): int =
             return i - 1
 
 
+func hitsArea(area: Area, velocity: Vector2D): bool =
+    let (y, x) = (area.y.a, area.x.b)
+    var probe = newProbe(velocity.y, velocity.x)
+    while true:
+        let pos = probe.position
+        if pos.y < y or pos.x > x:
+            return false
+        elif pos.y in area.y and pos.x in area.x:
+            return true
+        probe = step(probe)
+
+
+func findVelocities(area: Area): seq[Vector2D] =
+    let maxY = findHighestYVelocity(area)
+    for y in area.y.a .. maxY:
+        for x in 0 .. area.x.b:
+            let velocity = (y, x)
+            if hitsArea(area, velocity):
+                result.add(velocity)
+
+
 when isMainModule:
     import std/unittest
     suite "Day17":
@@ -72,11 +93,19 @@ when isMainModule:
                 doSteps(newProbe(2, 7), 4).position == (2, 22)
                 doSteps(newProbe(2, 7), 5).position == (0, 25)
 
-        test "example":
+        test "example 1":
             let area = parseInput("target area: x=20..30, y=-10..-5")
             check: findHighestYVelocity(area) == 9
+
+        test "example 2":
+            let area = parseInput("target area: x=20..30, y=-10..-5")
+            check findVelocities(area).len == 112
 
         test "solution 1":
             let area = parseInput("target area: x=282..314, y=-80..-45")
             let yVelocity = findHighestYVelocity(area)
             check doSteps(newProbe(yVelocity, 0), yVelocity).position.y == 3160
+
+        test "solution 2":
+            let area = parseInput("target area: x=282..314, y=-80..-45")
+            check findVelocities(area).len == 1928
